900fa8da06a8dc2347987951a8c9e827e1fc20dd
chatop <948905@qq.com>
2021-02-05 15:00:29 +0800

    更新rtsp地址校验正则表达式

--- LibCommon/UtilsHelper.cs
+++ LibCommon/UtilsHelper.cs
@@ -423,7 +423,17 @@ namespace LibCommon
         {
             try
             {
-                string Url = @"^rtsp(s)?://([\w-]+\.)+[\w-]+(:\d*)?(/[\w- ./?%&=]*)?$";
+                string Url = "^((rtsp|rtsps)?://)"
+                             + "?(([0-9a-zA-Z_!~*'().&=+$%-]+: )?[0-9a-zA-Z_!~*'().&=+$%-]+@)?" //rtsp的user@
+                             + "(([0-9]{1,3}.){3}[0-9]{1,3}" // IP形式的URL- 199.194.52.184
+                             + "|" // 允许IP和DOMAIN（域名）
+                             + "([0-9a-zA-Z_!~*'()-]+.)*" // 域名- www.
+                             + "([0-9a-zA-Z][0-9a-z-]{0,61})?[0-9a-z]." // 二级域名
+                             + "[a-zA-Z]{2,6})" // first level domain- .com or .museum
+                             + "(:[0-9]{1,4})?" // 端口- :80
+                             + "((/?)|" // a slash isn't required if there is no file name
+                             + "(/[0-9a-zA-Z_!~*'().;?:@&=+$,%#-]+)+/?)$";
+                // string Url = @"^rtsp(s)?://([\w-]+\.)+[\w-]+(:\d*)?(/[\w- ./?%&=]*)?$";
                 return Regex.IsMatch(str, Url);
             }
             catch
