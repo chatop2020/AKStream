fee2de2f8f04dab0aa363151f74a29f8d1534a84
chatop <948905@qq.com>
2021-02-22 09:47:09 +0800

    修改data[general][flowThreshold] = 0，使zlm能更准确触发onflowreport事件

--- AKStreamKeeper/MediaServerInstance.cs
+++ AKStreamKeeper/MediaServerInstance.cs
@@ -298,7 +298,7 @@ namespace AKStreamKeeper
                     data["hook"]["on_server_started"] = "";
                     //  $"http://{h}:{p}/MediaServer/WebHook/OnServerStarted"; //当流媒体启动时
                     data["hook"]["timeoutSec"] = "5"; //httpclient超时时间5秒
-                    data["general"]["flowThreshold"] = "1"; //当用户超过1byte流量时，将触发on_flow_report的webhook(/WebHook/OnStop)
+                    data["general"]["flowThreshold"] = "0"; //当用户超过1byte流量时，将触发on_flow_report的webhook(/WebHook/OnStop)
                     data["ffmpeg"]["bin"] = Common.AkStreamKeeperConfig.FFmpegPath;
                     data["ffmpeg"]["cmd"] = "%s -re -i %s -vcodec copy -acodec copy -f flv -y  %s";
                     data["ffmpeg"]["snap"] = "%s -i %s -y -f mjpeg -t 0.001 %s";
