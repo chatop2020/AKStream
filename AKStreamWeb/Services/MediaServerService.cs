af8eebb24223b0d7391eb2c61bb58886e2396452
chatop <948905@qq.com>
2021-06-20 21:53:20 +0800

    跟随zlm，对addstreamproxy接口增加retry_count参数，默认为-1:无限重试连接

--- AKStreamWeb/Services/MediaServerService.cs
+++ AKStreamWeb/Services/MediaServerService.cs
@@ -1672,6 +1672,7 @@ namespace AKStreamWeb.Services
             req.Enable_Mp4 = 0; //录制mp4要关
             req.Rtp_Type = videoChannel.RtpWithTcp == true ? 0 : 1; //rtsp拉流时，是否使用tcp,0为tcp,1为udp,2为组播
             req.Timeout_Sec = Common.AkStreamWebConfig.WaitEventTimeOutMSec / 1000; //超时时间
+            req.Retry_Count = -1;//无限重试
             var ret = mediaServer.WebApiHelper.AddStreamProxy(req, out rs);
             if (ret == null || !rs.Code.Equals(ErrorNumber.None) || ret.Code != 0)
             {
